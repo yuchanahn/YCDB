438c8e2acf013a5bff21ccdf6af6270b1c52be30
안유찬-main-pc <ultrauc123@gmail.com>
2021-09-16 23:27:30 +0900

    add linux server

--- YCDB/YCDB.cpp
+++ YCDB/YCDB.cpp
@@ -1,17 +1,7 @@
-﻿#include <iostream>
+﻿#include "server.hpp"
 
 
-
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	server_main(56789, [](char* msg, int len, int s) {
+		printf("%s\n", msg); });
 }
-
-
-
-// byte 자체를 key - value 형식으로 저장한다.
-// db 열람기능 있어야함....
-// db 수정 기능 있어야함.....
-// 크로스 플렛폼으로 제작할까?... 
--- YCDB/server.hpp
+++ YCDB/server.hpp
@@ -1,25 +1,47 @@
 #pragma once
+#include <iostream>
+#include <optional>
+#include <functional>
+
+#ifdef _WIN32
+// windows
+#pragma comment(lib, "ws2_32.lib")
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
-#include <iostream>
-#include <optional>
+#elif __APPLE__
+#include "TargetConditionals.h"
+#elif __linux__
+// linux
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <arpa/inet.h>
+#elif __unix__ // all unices not caught above
+// Unix
+#elif defined(_POSIX_VERSION)
+// POSIX
+#else
+#   error "Unknown compiler"
+#endif
 
 #define lmd(x) [&]{ return x; }
 
-auto error = [](auto&& condition, auto log, auto str) {
+auto error = [](auto condition, auto log, auto str) {
     bool b = condition();
-    if (!b) {
+    if (b) {
         if (log) log.value().append(str).append("\n");
         else log = str;
     }
     return b;
 };
 
-int server_main(int port) {
+int server_main(int port, std::function<void(char*,int,int)> read_func) {
+#ifdef _WIN32
     WSADATA wsaData;
-    SOCKET servSock, clntSock;   
+    SOCKET servSock, clntSock;
     SOCKADDR_IN servAddr, clntAddr;
     std::optional<std::string> elog;
 
@@ -41,21 +63,67 @@ int server_main(int port) {
     clntSock = accept(servSock, (sockaddr*)&clntAddr, &fromLen);
 
     if (!error(lmd(clntSock == INVALID_SOCKET), elog, "Accept Error")) {
-        printf("%s Connection Complete!\n", inet_ntoa(clntAddr.sin_addr));
         printf("Start ...\n");
     }
-
-    closesocket(servSock);
+    else {
+        closesocket(servSock);
+    }
 
     while (1) {
         int nRcv = recv(clntSock, message, sizeof(message) - 1, 0);
         if (error(lmd(nRcv == SOCKET_ERROR), elog, "Receive Error..")) break;
-        message[nRcv] = '\0';
-        send(clntSock, message, (int)strlen(message), 0);
+        read_func(message, nRcv, clntSock);
+        //send(clntSock, message, (int)strlen(message), 0);
     }
 
     closesocket(clntSock);
     WSACleanup();
+#elif __linux__
+    //================================================
+    unsigned int rl;
+    int list_sock;
+    int clnt_sock;
+    char buf[1024];
+    char bufSend[1024];
 
+    sockaddr_in server_addr, client_addr;
+    list_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    error(lmd(list_sock < 0), elog, "socket error");
+    int on = 1;
+    error(lmd(
+        setsockopt(list_sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&on, sizeof(on))
+        < 0),
+        elog, "socket opt error");
+
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    error(lmd(bind(list_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0), elog, "bind error");
+    error(lmd(listen(list_sock, 1024) < 0), elog, "listen error");
+    int clientAddrSize = sizeof(client_addr);
+    while (true)
+    {
+        clnt_sock = accept(list_sock, (struct sockaddr*)&client_addr, (socklen_t*)&clientAddrSize);
+        if (error(lmd(clnt_sock < 0), elog, "accept error")) return 1;
+
+        while (true)
+        {
+            memset(buf, 0, 1024);
+            rl = recv(clnt_sock, buf, 1024, 0);
+            if (rl == 0)
+            {
+                close(clnt_sock);
+                break;
+            }
+            if (rl > 1024)
+            {
+                close(clnt_sock);
+                break;
+            }
+            if (rl > 0) read_func(buf, rl, clnt_sock);
+        }
+    }
+    close(list_sock);
+#endif
     return 0;
-}
+};
