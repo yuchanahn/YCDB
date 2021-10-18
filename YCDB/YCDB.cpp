8a6bb6c1f0a3b0f04f97f5ea22c2198b4e2b1859
안유찬-main-pc <ultrauc123@gmail.com>
2021-10-18 23:29:57 +0900

    add packet_read 10%

--- YCDB/YCDB.cpp
+++ YCDB/YCDB.cpp
@@ -1,12 +1,30 @@
 ﻿#include "server.hpp"
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
+union uIntChar
+{
+	size_t uint;
+	char uchar[sizeof(size_t)];
+};
+
+std::vector<char> buffer;
 
 int main() {
 	server_main(56789, [](char* msg, int len, int s) {
+		uIntChar size;
+		if (len <= sizeof(size_t)) return;
+		std::copy(msg, msg+ sizeof(size_t), size.uchar);
 
-		printf("%s\n", msg); 
-		
-		
-		
+		if (len-sizeof(size_t) >= size.uint) {
+			for (int i = 0; i < size.uint; i++) msg[sizeof(size_t) + i];
+		}
+		else {
+			for (int i = 0; i < len - sizeof(size_t); i++) 
+				buffer.push_back(msg[sizeof(size_t) + i]);
+		}
+
+
 	});
 }
